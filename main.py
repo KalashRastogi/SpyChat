002e72c20a5c87601226adce395e92949e035c04
KalashRastogi <kalashrastogi4@gmail.com>
2018-01-02 01:39:02 +0530

    main.py added starting of spy chat project

---
+++ main.py
@@ -0,0 +1,12 @@
+print "Welcome!! to SPY CHAT"
+print "Let's get Started!"
+
+spy_name = raw_input("What is your Name?")
+spy_salutation = raw_input("What should we call you(Mr./Mrs./Miss)?")
+
+spy_name = spy_salutation + " " + spy_name
+
+print "Welcome!! " + spy_name + ".\nGlad to have you with us."
+
+
+
