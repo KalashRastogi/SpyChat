df9ac017a8bc53aee3b9ed54419ec797255b1ff3
KalashRastogi <kalashrastogi4@gmail.com>
2018-01-05 20:24:49 +0530

    Spychat - first module completed

--- main.py
+++ main.py
@@ -1,29 +1,25 @@
 #SPYCHAT project
 
+#MPORTING VARIABLES FROM SPY_DETAILS FILE
+
+from spy_details import spy_name,spy_salutation,spy_rating,spy_age,spy_is_online
+
 print "Welcome!! to SPY CHAT"
 print "Let's get Started!"
 
-spy_name = raw_input("What is your Name?")
+#asking user about if he wants to conitue with the same name and age as previously logged in
 
-if len(spy_name) > 0:  # to check whether user eneterd something or not
+question = "Do you want to continue as %s %s(Y/N)? " %(spy_salutation,spy_name)
+existing=raw_input(question)
 
-    spy_salutation = raw_input("What should we call you(Mr./Mrs./Miss)?")
+def start_chat(spy_name,spy_salutation,spy_age,spy_rating):
 
-    spy_name = spy_salutation + " " + spy_name  # concatenation
+    current_status_message = None
 
-    print "Welcome!! " + spy_name + ".\nGlad to have you with us."
+    spy_name = spy_salutation + " " + spy_name
 
-    print "Alright " + spy_name + ". I'd like to know a little bit more about you before we proceed..."
-
-    spy_age=0
-    spy_rating=0.0
-    spy_is_online=False
-
-    spy_age = input("What's your Age?")
 
     if spy_age > 12 and spy_age < 50:
-
-        spy_rating=input("Enter ratings?(0-5)")
 
         if spy_rating > 4.5 and spy_rating <= 5.0:
             print "Great Ace!!"
@@ -37,13 +33,56 @@ if len(spy_name) > 0:  # to check whether user eneterd something or not
         else:
             print "We can always use somebody to help in the office."
 
+        print "Authentication complete. Welcome %s Age: %d and Rating of: %.2f \nProud to have you onboard" % (
+        spy_name, spy_age, spy_rating)
+
         spy_is_online=True
 
-        print "Authentication complete. Welcome " + spy_name + " age: " + str(spy_age) + " and rating of: " + str(spy_rating) + " Proud to have you onboard"
+        while(True):
+
+            print "1.STATUS UPDATE"
+            print "2.EXIT"
+            choice = raw_input("ENTER YOUR CHOICE:")
+            if choice == "1":
+
+                #it is not yet do any work
+                print "something"
+
+            else:
+
+                exit(0)
+
 
     else:
         print "You are not of the correct age to be a SPY!!"
 
+
+if existing == "Y" or existing =="y":
+    start_chat(spy_name,spy_salutation,spy_age,spy_rating)
+
 else:
 
-    print("Sorry!! A Spy need to have a Valid Name.Try Again Please!!")
+    # ducktyping
+
+    spy_age = 0
+    spy_rating = 0.0
+    spy_is_online = False
+
+    spy_name = raw_input("Welcome! to SpyChat You must tell me your name first:")
+
+    # to check whether user enetered something or not
+    if len(spy_name) > 0:
+
+        spy_salutation = raw_input("What should we call you(Mr./Mrs./Miss)?")
+
+        spy_age = input("What's your Age:")
+
+        spy_rating = input("What is your Spy Rating:")
+
+        spy_is_online = True
+
+        start_chat(spy_name,spy_salutation,spy_age,spy_rating)
+
+    else:
+
+        print("Sorry!! A Spy need to have a Valid Name.Try Again Please!!")
---
+++ spy_details.py
@@ -0,0 +1,5 @@
+spy_name="Jon Snow"
+spy_age=32
+spy_salutation="Mr."
+spy_rating=4.9
+spy_is_online=True
